1d8b3a3291399cd1d3c876e93409b0fe9f062032
Azael Contramaestre <galaxyhhd@hotmail.com>
2021-03-09 16:05:55 -0300

    added cute things

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React, {Fragment} from "react"
+
+
+export const About = () => {
+    return (
+        <Fragment>
+            <p>
+               Voluptate minim minim velit laborum culpa non ad commodo. Ipsum ullamco eiusmod fugiat nulla cillum incididunt in. Veniam amet nulla aute eiusmod magna commodo eiusmod. Eiusmod in sint id esse commodo dolor esse deserunt in culpa. Fugiat et veniam cupidatat aute culpa nulla ea.
+
+Magna ex id mollit do sint qui aliqua adipisicing dolore velit in. Veniam elit anim et amet irure. Laboris aliqua commodo amet laboris proident adipisicing amet eu sunt. Adipisicing enim id veniam pariatur esse nulla minim nisi occaecat sunt cillum sunt veniam nulla. Consequat et occaecat pariatur est cillum culpa quis enim mollit ad. Et nisi aliqua sint occaecat nostrud tempor occaecat ex ex consequat. Aliqua aliqua dolore sunt est consectetur.
+
+Elit laboris cillum nisi dolore minim officia commodo ut aute ut exercitation velit cupidatat quis. Laboris consequat id ex ipsum adipisicing. Ullamco ullamco deserunt veniam magna eiusmod anim. Quis labore consectetur nostrud esse deserunt. Non voluptate est cupidatat anim excepteur laboris ullamco ullamco exercitation excepteur magna occaecat reprehenderit. Voluptate incididunt fugiat ea qui eiusmod commodo incididunt aliqua consequat pariatur minim qui velit.
+
+Sunt amet est occaecat ea pariatur culpa fugiat commodo ut nulla voluptate aute. Elit mollit excepteur cupidatat ea amet ipsum occaecat officia. Ut laborum tempor id veniam reprehenderit commodo cillum.
+
+Enim in commodo do voluptate Lorem eu qui ea qui. Est aliquip esse ex nostrud culpa consequat aliquip culpa consectetur in ad. Ea fugiat aute nostrud anim. Cillum dolor reprehenderit in sunt cillum. Nostrud est exercitation qui esse in dolor ex Lorem laborum. Fugiat irure ad ad aute ipsum nostrud laborum mollit tempor velit ea magna anim. Aliqua ut et nisi labore officia.
+
+Mollit cillum incididunt voluptate incididunt in. Adipisicing in ut exercitation et ad. Dolore ex excepteur eiusmod minim adipisicing excepteur voluptate proident esse do elit Lorem. Minim velit aliquip aute aliquip nostrud sint fugiat in ex. Esse culpa exercitation culpa enim. Cillum et anim minim nulla. Id do nulla aute irure quis.
+
+Ad irure est adipisicing ullamco officia culpa aliquip velit. Culpa quis cillum duis elit. Esse qui ipsum sint cupidatat ipsum laboris aute dolor incididunt.
+
+Ad est duis eu et duis in proident enim. Deserunt enim Lorem dolor culpa commodo consectetur anim proident enim ad laborum in incididunt. Do ullamco consequat mollit nostrud sit labore.
+
+In labore magna dolor anim incididunt aliqua voluptate. Deserunt adipisicing tempor elit adipisicing. Pariatur laboris consectetur cupidatat veniam ad laborum ut anim. Sint officia labore esse sit cillum irure. In occaecat ea eu exercitation quis in aliqua veniam. Nostrud officia exercitation est dolor.
+            </p>
+            <p>
+                Minim enim cupidatat est exercitation magna nisi ipsum sunt ullamco et in enim. Exercitation non ad sunt nisi fugiat eiusmod. Excepteur nostrud consectetur veniam dolor. Pariatur veniam officia voluptate sit. Cupidatat laborum non irure sunt. Occaecat duis eiusmod eiusmod ipsum consequat ex magna aliquip reprehenderit tempor ut officia quis adipisicing. Pariatur ad enim laboris deserunt.
+            </p>
+        </Fragment>
+        )
+}
